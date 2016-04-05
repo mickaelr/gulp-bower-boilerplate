38018e4c92075b0511710f094256d4320caea18a
Mickael Roy <roy-mickael@live.fr>
2016-04-06 00:11:26 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Gulp & Bower boilerplate
+
+A simple project boilerplate using Guld and Bower
+
+## Installation
+
+Needs Node & NPM installed.
+Launch a terminal and go to the project directory.
+```
+npm install
+```
+```
+bower install
+```
+```
+gulp
+```
+
+## License
+
+The plugin is under MIT License
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp')
+    uglify = require('gulp-uglify')
+    sass = require('gulp-sass');
+
+// Styles task
+gulp.task('styles', function() {
+  gulp.src('src/**/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('dist/'));
+});
+
+// Scripts task
+gulp.task('scripts', function() {
+  gulp.src('src/**/*.js')
+  .pipe(uglify())
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(gulp.dest('dist/'))
+});
+
+// Watch task
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.scss', ['styles']);
+  gulp.watch('src/**/*.js', ['scripts']);
+});
+
+gulp.task('default', ['styles', 'scripts', 'watch']);
